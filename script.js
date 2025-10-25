88d2ae6e45ceb78ddab4f9e80e45e04c0b947210
Sam Daigle <110954530+samdaigle@users.noreply.github.com>
2025-10-25 13:47:21 -0400

    add basic math functions to js file

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(1, 2));
+console.log(subtract(10, 4));
+console.log(multiply(3, 5));
+console.log(divide(6, 7));
